c04ec2c13a153f701a7a9be75630bfd93f1f86d9
XiangZheng <31711517+XiangZheng2017@users.noreply.github.com>
2017-11-19 13:23:48 -0500

    Add files via upload

--- grabcutios-master/GrabCutIOS/GrabCutIOS/ViewController.h
+++ grabcutios-master/GrabCutIOS/GrabCutIOS/ViewController.h
@@ -9,11 +9,11 @@
 
 #import <UIKit/UIKit.h>
 
-#import <opencv2/opencv.hpp>
+//#import <opencv2/opencv.hpp>
 #import <opencv2/imgproc/imgproc_c.h>
 #import <opencv2/highgui/highgui_c.h>
 #import <opencv2/core/core_c.h>
-using namespace cv;
+//using namespace cv;
 
 @interface ViewController : UIViewController
 
