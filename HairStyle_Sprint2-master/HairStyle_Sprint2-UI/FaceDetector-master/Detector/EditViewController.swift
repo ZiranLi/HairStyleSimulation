c5b8c2991180300260b3f2da65fd3e1612f7acfa
YinzhuSU <31676438+YinzhuSU@users.noreply.github.com>
2017-11-19 17:43:05 -0500

    Launch in and Copyrights

---
+++ HairStyle_Sprint2-master/HairStyle_Sprint2-UI/FaceDetector-master/Detector/EditViewController.swift
@@ -0,0 +1,36 @@
+//
+//  EditViewController.swift
+//  Detector
+//
+//  Created by Xiang Zheng on 10/27/17.
+//  Copyright © 2017 Gregg Mojica. All rights reserved.
+//
+
+import UIKit
+
+class EditViewController: UIViewController {
+
+    @IBOutlet weak var imageView: UIImageView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
